8cc33d111644b4b4f4a1e2ca062373812ee87515
1nTh35h3ll <yossis@protonmail.com>
2024-09-20 23:03:31 +0300

    v1.0.4 - added support for ConfigurationName, ProcessID and session state + fixed delay bug with non-responsive computer(s)

--- Get-RemotePSSession.ps1
+++ Get-RemotePSSession.ps1
@@ -1,9 +1,11 @@
 function Global:Get-RemotePSSession
 {
-# v1.0.3 - added support for Computername parameter to query multiple hosts in one command run
+# v1.0.4 - added support for ConfigurationName, ProcessID and session state + fixed delay bug with non-responsive computer(s)
+# v1.0.3 - added support for Computername parameter to query multiple hosts in a single command run
 # v1.0.2 - added error handling and better indication for access denied etc.
 # v1.0.1 - added wincompat shell name to identify local PWSH runspaces
 # comments to yossis@protonmail.com
+
 [CmdletBinding()]
     Param
     (
@@ -37,6 +39,9 @@ public string ConnectionURI;
 public bool UseSSL=false;
 public string Name;
 public string RemoteComputer;
+public string ConfigurationName;
+public string ProcessId;
+public string State;
             }}
 "@
     } else {
@@ -53,6 +58,9 @@ public string ConnectionURI;
 public bool UseSSL=false;
 public string Name;
 public string RemoteComputer;
+public string ConfigurationName;
+public string ProcessId;
+public string State;
             }}
 "@
     }
@@ -64,24 +72,40 @@ public string RemoteComputer;
     { 
 
 # Set port
-$Port = if ($UseSSL) {5986} else {5985}
-$results = @();
+$global:Port = if ($UseSSL) {5986} else {5985}
 
-$ComputerName | ForEach-Object {
-$Computer = $_; 
-$URI = "http://$($Computer):$port/wsman";
-
-# enum sessions
+# declare port ping function & some variables for error handling + results array
+filter Invoke-PortPing {((New-Object System.Net.Sockets.TcpClient).ConnectAsync($_,$global:Port)).Wait(100)}
 $EAP = $ErrorActionPreference;
 $ErrorActionPreference = "SilentlyContinue";
+$results = @();
+$AvailableComputers = @();
+
+# first check if host(s) is/are available on winrm port
+$ComputerName | ForEach-Object {
+    $Computer = $_;
+    if (($Computer | Invoke-PortPing) -eq $False) 
+        {
+            Write-Host "Computer $($Computer.ToUpper()) is not responsive on port $global:Port. Skipping." -ForegroundColor Yellow
+        }
+    else
+        {
+            $AvailableComputers += $Computer
+        }
+}
+
+# Get session(s) information
+$AvailableComputers | ForEach-Object {
+$Computer = $_; 
+
+# enum sessions
+$URI = "http://$($Computer):$global:Port/wsman";
 $sessions = Get-WSManInstance -ConnectionURI $URI shell -Enumerate;
 
 # handle errors, abort if no access
 if (!$?) {
     Write-Warning "An error occured while trying to access $URI";
     $Error[0].exception;
-    $ErrorActionPreference = $EAP;
-    break
 }
 
 if ($sessions -ne $null) {
@@ -100,11 +124,14 @@ foreach($session in $sessions)
             $obj.ConnectionURI = $uri
             $obj.UseSSL = $UseSSL
 	        $obj.Name = $session.Name
-            $obj.RemoteComputer = $Computer
+            $obj.RemoteComputer = $Computer.ToUpper()
+            $obj.ConfigurationName = $session.ResourceURI.Split("/")[-1]
+            $obj.ProcessId = $session.ProcessId
+            $obj.State = $session.State
             $results += $obj
         }
       }
-      else {Write-Host "No Remote PSSessions found on $Computer on port $Port." -ForegroundColor Cyan}
+      else {Write-Host "No Sessions found on $($Computer.ToUpper()) on port $global:Port." -ForegroundColor Cyan}
     }}
     End
     {        
