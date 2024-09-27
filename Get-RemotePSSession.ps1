eab4bc76a213b55fdf351765d6d8ce553ded0407
1nTh35h3ll <yossis@protonmail.com>
2024-09-27 07:28:31 +0300

    v1.0.5 - added Port parameter (in addition to UseSSL option/5986) + simplified .net class add-type statement

--- Get-RemotePSSession.ps1
+++ Get-RemotePSSession.ps1
@@ -1,5 +1,6 @@
 function Global:Get-RemotePSSession
 {
+# v1.0.5 - added Port parameter (in addition to UseSSL option/5986) + simplified .net class add-type statement
 # v1.0.4 - added support for ConfigurationName, ProcessID and session state + fixed delay bug with non-responsive computer(s)
 # v1.0.3 - added support for Computername parameter to query multiple hosts in a single command run
 # v1.0.2 - added error handling and better indication for access denied etc.
@@ -14,8 +15,10 @@ function Global:Get-RemotePSSession
                 ValueFromPipeline=$true,
                 Position=0)]
                 [string[]]$ComputerName, 
-     # use SSL
+     # Use SSL (5986)
      [switch]$UseSSL,
+     # Optional, to specify port number
+     [int]$Port,
      # Resolve IPs to hostnames
      [switch]$ResolveClientHostname
     )
@@ -23,11 +26,9 @@ function Global:Get-RemotePSSession
     Begin
     {
         try{
-            if ($ResolveClientHostname)
-            {
             Add-Type  @"
 namespace GetRemotePSSession{
-public class PSSession1
+public class PSSessionInfo
             {
 public string Owner;
 public string ClientIP;
@@ -36,34 +37,15 @@ public string SessionTime;
 public string IdleTime;
 public string ShellID;
 public string ConnectionURI;
-public bool UseSSL=false;
+public bool UseSSL;
 public string Name;
 public string RemoteComputer;
 public string ConfigurationName;
 public string ProcessId;
 public string State;
+public string Port;
             }}
 "@
-    } else {
-    Add-Type  @"
-namespace GetRemotePSSession{
-public class PSSession2
-            {
-                public string Owner;
-public string ClientIP;
-public string SessionTime;
-public string IdleTime;
-public string ShellID;
-public string ConnectionURI;
-public bool UseSSL=false;
-public string Name;
-public string RemoteComputer;
-public string ConfigurationName;
-public string ProcessId;
-public string State;
-            }}
-"@
-    }
         }
         catch{}
         $results = @() 
@@ -72,10 +54,11 @@ public string State;
     { 
 
 # Set port
-$global:Port = if ($UseSSL) {5986} else {5985}
+[int]$global:PortNumber = if ($UseSSL) {5986} else {5985}
+if ($Port) {$global:PortNumber = $Port}
 
 # declare port ping function & some variables for error handling + results array
-filter Invoke-PortPing {((New-Object System.Net.Sockets.TcpClient).ConnectAsync($_,$global:Port)).Wait(100)}
+filter Invoke-PortPing {((New-Object System.Net.Sockets.TcpClient).ConnectAsync($_,$global:PortNumber)).Wait(100)}
 $EAP = $ErrorActionPreference;
 $ErrorActionPreference = "SilentlyContinue";
 $results = @();
@@ -86,7 +69,7 @@ $ComputerName | ForEach-Object {
     $Computer = $_;
     if (($Computer | Invoke-PortPing) -eq $False) 
         {
-            Write-Host "Computer $($Computer.ToUpper()) is not responsive on port $global:Port. Skipping." -ForegroundColor Yellow
+            Write-Host "Computer $($Computer.ToUpper()) is not responsive on port $global:PortNumber. Skipping." -ForegroundColor Yellow
         }
     else
         {
@@ -99,7 +82,7 @@ $AvailableComputers | ForEach-Object {
 $Computer = $_; 
 
 # enum sessions
-$URI = "http://$($Computer):$global:Port/wsman";
+$URI = "http://$($Computer):$global:PortNumber/wsman";
 $sessions = Get-WSManInstance -ConnectionURI $URI shell -Enumerate;
 
 # handle errors, abort if no access
@@ -111,27 +94,28 @@ if (!$?) {
 if ($sessions -ne $null) {
 foreach($session in $sessions)
         {
-            if ($ResolveClientHostname) {$obj = New-Object GetRemotePSSession.PSSession1} else {$obj = New-Object GetRemotePSSession.PSSession2}
-            $obj.Owner = $session.owner
-            $obj.ClientIP = $session.clientIp
+            $obj = New-Object GetRemotePSSession.PSSessionInfo;
+            $obj.Owner = $session.owner;
+            $obj.ClientIP = $session.clientIp;
 	        if ($ResolveClientHostname) {
                 if ($obj.clientIP -eq "::1" -xor $obj.clientIP -eq "127.0.0.1") {$obj.ClientHostname = $Computer}
                 else {$obj.ClientHostname = [System.Net.Dns]::GetHostEntry($obj.ClientIP).HostName.ToUpper()}
             }
-            $obj.SessionTime = [System.Xml.XmlConvert]::ToTimeSpan($session.shellRunTime).tostring()
-            $obj.IdleTime = [System.Xml.XmlConvert]::ToTimeSpan($session.shellInactivity).tostring()
-            $obj.ShellID = $session.shellid
-            $obj.ConnectionURI = $uri
-            $obj.UseSSL = $UseSSL
-	        $obj.Name = $session.Name
-            $obj.RemoteComputer = $Computer.ToUpper()
-            $obj.ConfigurationName = $session.ResourceURI.Split("/")[-1]
-            $obj.ProcessId = $session.ProcessId
-            $obj.State = $session.State
+            $obj.SessionTime = [System.Xml.XmlConvert]::ToTimeSpan($session.shellRunTime).tostring();
+            $obj.IdleTime = [System.Xml.XmlConvert]::ToTimeSpan($session.shellInactivity).tostring();
+            $obj.ShellID = $session.shellid;
+            $obj.ConnectionURI = $uri;
+            if ($UseSSL) {$obj.UseSSL = $true} else {$UseSSL = $false}
+	        $obj.Name = $session.Name;
+            $obj.RemoteComputer = $Computer.ToUpper();
+            $obj.ConfigurationName = $session.ResourceURI.Split("/")[-1];
+            $obj.ProcessId = $session.ProcessId;
+            $obj.State = $session.State;
+            $obj.Port = $global:PortNumber;
             $results += $obj
         }
       }
-      else {Write-Host "No Sessions found on $($Computer.ToUpper()) on port $global:Port." -ForegroundColor Cyan}
+      else {Write-Host "No Sessions found on $($Computer.ToUpper()) on port $global:PortNumber." -ForegroundColor Cyan}
     }}
     End
     {        
